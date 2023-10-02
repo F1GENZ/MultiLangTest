0a3c5d125dedb5fa1bfa5b7b261cc628d58ede07
HangFamily <hangquoctai.family@gmail.com>
2023-10-02 21:26:30 +0700

    Upload GlobalService

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -7,7 +7,7 @@ export class ShopController {
 
   @Get('/install/login')
   @Redirect(
-    `https://accounts.haravan.com/connect/authorize?response_mode=${globalService.globalVar.response_mode}&response_type=${globalService.globalVar.response_type}&scope=${globalService.globalVar.scope_login}&client_id=${globalService.globalVar.app_id}&redirect_uri=${globalService.globalVar.login_callback_url}&nonce=${globalService.globalVar.nonce}&orgid=${globalService.globalVar.orgid}`,
+    `https://accounts.haravan.com/connect/authorize?response_mode=${globalService.globalVar.response_mode}&response_type=${globalService.globalVar.response_type}&scope=${globalService.globalVar.scope_login}&client_id=${globalService.globalVar.app_id}&redirect_uri=${globalService.globalVar.login_callback_url}&nonce=${globalService.globalVar.nonce}`,
   )
   installLogin(@Req() req) {
     console.log(req);
